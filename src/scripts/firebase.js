d832dcc72ed82229a531053bf65de9b276ea7dc6
lauraduza9857 <80287236+lauraduza9857@users.noreply.github.com>
2022-05-11 01:30:20 -0500

    redireccionar

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -53,8 +53,14 @@ async function createUser(name,username,email,password){
     try{
     const newUser =await createUserWithEmailAndPassword(auth, email, password);
     console.log(newUser);
+    alert("Usuario registrado exitosamente");
+    window.location.replace("../shop.html");
     }catch(e){
         console.log(e);
+        if(e.code === "auth/weak-password"){
+            alert("La contraseña debe contener por lo menos 6 letras o números");
+        }
+        
     }
 };
 
@@ -63,10 +69,18 @@ async function login(email, password){
     try{
 
         const { user }= await signInWithEmailAndPassword(auth, email, password);
-        console.log("ingreso exitoso!");
+        alert("ingreso exitoso, bienvenidx!");
+        window.location.replace("../shop.html");
 
     }catch(e){
         console.log(e);
+        if(e.code === "auth/wrong-password"){
+            alert("contraseña Incorrecta");
+        }
+        if(e.code === "auth/user-not-found"){
+            alert("Este usuario no se encuentra en la base de datos");
+        }
+        
     }
 };
 
