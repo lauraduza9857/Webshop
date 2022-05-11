00aac014fd0fba7660503ef49e0bc86cc56c6701
lauraduza9857 <80287236+lauraduza9857@users.noreply.github.com>
2022-05-11 00:09:20 -0500

    Pude crear usuarios

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,6 +1,8 @@
 //Aqui va a quedar la conexión de firebase
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+//Cosas necesarias para realizar la autenticación
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,10 +18,29 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//constante para la autenticación
+const auth = getAuth();
 
 // Creo un listener del formulario de crear usuario nuevo
 const createUserForm = document.getElementById("createUserForm");
 createUserForm.addEventListener("submit", e =>{
     e.preventDefault();
-    console.log("Usuario Creado");
-})
+   // console.log("Usuario Creado");
+   //cuando el usurio oprima el boton de submit llamamos la funcion que crea el usurio
+   
+   const name = createUserForm.name.value;
+   const username = createUserForm.username.value;
+   const email = createUserForm.email.value;
+   const password = createUserForm.password.value;
+   createUser(name, username, email, password);
+});
+
+async function createUser(name,username,email,password){
+    try{
+    const newUser =await createUserWithEmailAndPassword(auth, email, password);
+    console.log(newUser);
+    }catch(e){
+        console.log(e);
+    }
+}
+
